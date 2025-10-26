35c0fd1d26defeb7004a23c8755bc4152b2d6113
Pravallika11112345678 <pravallikauddarraju@gmail.com>
2025-10-26 19:38:52 +0530

    updated...

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Problem Statement: Print "Hello World" in Java
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ hello.c
@@ -0,0 +1,10 @@
+// Problem Statement: Print "Hello World" in C
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
